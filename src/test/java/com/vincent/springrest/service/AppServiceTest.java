9c731e0c3a286b48f9993565cda35faa31168884
Vincent <ye.zheng@mail.utoronto.ca>
2016-12-25 12:08:26 -0500

    Update the AppService Test

--- src/test/java/com/vincent/springrest/service/AppServiceTest.java
+++ src/test/java/com/vincent/springrest/service/AppServiceTest.java
@@ -40,13 +40,12 @@ public class AppServiceTest {
 				new LocalDate(1991, 7, 15));
 		List<User> expected = Arrays.asList(user1, user2);
 		Mockito.when(userDAO.findByUsername("admin")).thenReturn(user1);
+		Mockito.when(userDAO.findById(1)).thenReturn(user1);
 		Mockito.when(userDAO.findAllUsers()).thenReturn(expected);
 	}
 
 	@After
 	public void verify() {
-		Mockito.verify(userDAO, VerificationModeFactory.times(1)).findByUsername(Mockito.anyString());
-		//Mockito.verify(userDAO, VerificationModeFactory.times(1)).findAllUsers();
 		// This is allowed here: using container injected mocks
 		Mockito.reset(userDAO);
 	}
@@ -60,23 +59,45 @@ public class AppServiceTest {
 		assertEquals("Admin", user.getLastName());
 		assertEquals("vincentcheng787@gmail.com", user.getEmail());
 		assertEquals("1990-12-01", user.getDateOfBirth().toString());
+		Mockito.verify(userDAO, VerificationModeFactory.times(1)).findByUsername(Mockito.anyString());
 	}
 	
 	@Test
 	public void testFindByUsernameFail(){
 		User user = userService.findByUsername("yukirin");
 		assertNull(user);
+		Mockito.verify(userDAO, VerificationModeFactory.times(1)).findByUsername(Mockito.anyString());
+	}
+	
+	@Test
+	public void testFindByIdSuccess(){
+		User user = userService.findById(1);
+		assertEquals((Integer)1, user.getId());
+		assertEquals("admin", user.getUsername());
+		assertEquals("Admin", user.getFirstName());
+		assertEquals("Admin", user.getLastName());
+		assertEquals("vincentcheng787@gmail.com", user.getEmail());
+		assertEquals("1990-12-01", user.getDateOfBirth().toString());
+		Mockito.verify(userDAO, VerificationModeFactory.times(1)).findById(Mockito.anyInt());
+	}
+	
+	@Test
+	public void testFindByIdFail(){
+		User user = userService.findById(2);
+		assertNull(user);
+		Mockito.verify(userDAO, VerificationModeFactory.times(1)).findById(Mockito.anyInt());
 	}
 
-//	@Test
-//	public void testFindAll(){
-//		List<User> users = userService.findAllUsers();
-//		User user1 = new User(1, "admin", "admin", "Admin", "Admin", "vincentcheng787@gmail.com", 
-//				new LocalDate(1990, 12, 1));
-//		User user2 = new User(2, "yukirin", "Yuki0715", "Yuki", "Kashiwagi", "yuki.kashiwagi@akb.co.jp", 
-//				new LocalDate(1991, 7, 15));
-//		assertEquals(Arrays.asList(user1, user2), users);
-//	}
+	@Test
+	public void testFindAll(){
+		List<User> users = userService.findAllUsers();
+		User user1 = new User(1, "admin", "admin", "Admin", "Admin", "vincentcheng787@gmail.com", 
+				new LocalDate(1990, 12, 1));
+		User user2 = new User(2, "yukirin", "Yuki0715", "Yuki", "Kashiwagi", "yuki.kashiwagi@akb.co.jp", 
+				new LocalDate(1991, 7, 15));
+		assertEquals(Arrays.asList(user1, user2), users);
+		Mockito.verify(userDAO, VerificationModeFactory.times(1)).findAllUsers();
+	}
 
 	@Configuration
 	static class UserServiceTestContextConfiguration {
