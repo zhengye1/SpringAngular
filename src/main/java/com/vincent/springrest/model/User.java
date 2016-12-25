b69ccda96e1bbc47f1c1fc513525c95dfab93e03
Vincent <ye.zheng@mail.utoronto.ca>
2016-12-25 00:03:40 -0500

    Update the model to use serialize only

--- src/main/java/com/vincent/springrest/model/User.java
+++ src/main/java/com/vincent/springrest/model/User.java
@@ -24,7 +24,7 @@ public class User implements Serializable {
 	private String lastName;
 	private String email;
 	
-    @JsonDeserialize(using = CustomDateDeserializer.class)
+    //@JsonDeserialize(using = CustomDateDeserializer.class)
 	@JsonSerialize(using = CustomDateSerializer.class)
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
 	private LocalDate dateOfBirth;
