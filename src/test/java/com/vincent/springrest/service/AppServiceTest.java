b698312cb5c794c1ed0528eef668bb855d56b392
Vincent <ye.zheng@mail.utoronto.ca>
2016-12-25 02:10:11 -0500

    Fixed the indentation

--- src/test/java/com/vincent/springrest/service/AppServiceTest.java
+++ src/test/java/com/vincent/springrest/service/AppServiceTest.java
@@ -22,50 +22,50 @@ import com.vincent.springrest.model.User;
 @ContextConfiguration
 public class AppServiceTest {
 
-	
+
 	@Autowired
 	UserService userService; 
-	
+
 	@Autowired
 	UserDAO userDAO;
-	
-    @Before
-    public void setup() {
-    	User user = new User(1, "admin", "admin", "Admin", "Admin", "vincentcheng787@gmail.com", 
+
+	@Before
+	public void setup() {
+		User user = new User(1, "admin", "admin", "Admin", "Admin", "vincentcheng787@gmail.com", 
 				new LocalDate(1990, 12, 1));
-        Mockito.when(userDAO.findByUsername("admin")).thenReturn(user);
-    }
+		Mockito.when(userDAO.findByUsername("admin")).thenReturn(user);
+	}
 
-    @After
-    public void verify() {
-        Mockito.verify(userDAO, VerificationModeFactory.times(1)).findByUsername(Mockito.anyString());
-        // This is allowed here: using container injected mocks
-        Mockito.reset(userDAO);
-    }
-    
-    @Test
-    public void testFindByUsernameSuccess(){
-    	User user = userService.findByUsername("admin");
-    	assertEquals((Integer)1, user.getId());
-    	assertEquals("admin", user.getUsername());
-    	assertEquals("Admin", user.getFirstName());
-    	assertEquals("Admin", user.getLastName());
-    	assertEquals("vincentcheng787@gmail.com", user.getEmail());
-    	assertEquals("1990-12-01", user.getDateOfBirth().toString());
-    }
-    
-    
-    @Configuration
-    static class UserServiceTestContextConfiguration {
+	@After
+	public void verify() {
+		Mockito.verify(userDAO, VerificationModeFactory.times(1)).findByUsername(Mockito.anyString());
+		// This is allowed here: using container injected mocks
+		Mockito.reset(userDAO);
+	}
 
-        @Bean
-        public UserService userService() {
-            return new UserServiceImpl();
-        }
+	@Test
+	public void testFindByUsernameSuccess(){
+		User user = userService.findByUsername("admin");
+		assertEquals((Integer)1, user.getId());
+		assertEquals("admin", user.getUsername());
+		assertEquals("Admin", user.getFirstName());
+		assertEquals("Admin", user.getLastName());
+		assertEquals("vincentcheng787@gmail.com", user.getEmail());
+		assertEquals("1990-12-01", user.getDateOfBirth().toString());
+	}
 
-        @Bean
-        public UserDAO userDAO() {
-            return Mockito.mock(UserDAO.class);
-        }
-    }
+
+	@Configuration
+	static class UserServiceTestContextConfiguration {
+
+		@Bean
+		public UserService userService() {
+			return new UserServiceImpl();
+		}
+
+		@Bean
+		public UserDAO userDAO() {
+			return Mockito.mock(UserDAO.class);
+		}
+	}
 }
